febc42adc744a67cb979de9160a1464b3260826b
boomchanotai <boom9387@hotmail.com>
2024-04-17 15:45:29 +0700

    feat: sse subscribe

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -2,8 +2,29 @@ const getRoom = async (req, res) => {
   res.send("Room Controller");
 };
 
+const subscribers = {};
 const subscribeChat = async (req, res) => {
-  res.send("Subscribe Chat Controller");
+  const user = req.headers["user"];
+  const roomId = req.params.id;
+
+  if (!subscribers[roomId]) {
+    subscribers[roomId] = [];
+  }
+
+  subscribers[roomId].push(res);
+
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+
+  res.writeHead(200, headers);
+  res.write(`data: ${user} connected\n\n`);
+
+  req.on("close", () => {
+    res.end();
+  });
 };
 
 module.exports = { getRoom, subscribeChat };
