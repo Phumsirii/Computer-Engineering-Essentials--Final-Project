e5eada345726d667226648562c3e2e44c8a7f104
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-04-19 10:04:41 +0700

    create room CRUD

--- backend/routes/roomRoute.js
+++ backend/routes/roomRoute.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const {
-  getRoom,
   subscribeChat,
   postDraw,
+  createRoom,
+  getRoom,
+  updateRoom,
+  deleteRoom,
 } = require("../controllers/roomController");
 
 const router = express.Router();
 
-router.get("/", getRoom);
+// router.get("/", getRoom);
 router.get("/:id/subscribe", subscribeChat);
 router.post("/:id/draw", postDraw);
+router.post("/", createRoom);
+router.route("/:id").get(getRoom).put(updateRoom).delete(deleteRoom);
 
 module.exports = router;
